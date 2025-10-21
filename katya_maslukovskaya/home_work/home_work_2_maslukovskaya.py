6cb9c424cd5667103293501611fcc09827a69f8c
kmaslukovskaya <k.maslukovskaya@gmail.com>
2025-10-21 17:41:35 +0300

    Домашка 2. Assert

--- katya_maslukovskaya/home_work/home_work_2_maslukovskaya.py
+++ katya_maslukovskaya/home_work/home_work_2_maslukovskaya.py
@@ -1,7 +1,6 @@
 import requests
 
 url="https://swapi.dev/api/planets/3/"
-
 response = requests.request("GET", url)
 response_json=response.json()
 assert response_json['population']=='1000',f'Популяция не равно 1000, а равно {response_json['population']}'
