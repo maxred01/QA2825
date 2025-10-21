baf2d4654d9cac51fd011ead6e66aa20911ecb30
kmaslukovskaya <k.maslukovskaya@gmail.com>
2025-10-21 17:39:43 +0300

    Домашка 2. Assert

--- katya_maslukovskaya/home_work/home_work_2_maslukovskaya.py
+++ katya_maslukovskaya/home_work/home_work_2_maslukovskaya.py
@@ -4,6 +4,5 @@ url="https://swapi.dev/api/planets/3/"
 
 response = requests.request("GET", url)
 response_json=response.json()
-
 assert response_json['population']=='1000',f'Популяция не равно 1000, а равно {response_json['population']}'
 assert response_json['created']=='2014-12-10T11:37:19.144000Z',f'Поле создано не соответствует.Должно быть {response_json['created']}'
