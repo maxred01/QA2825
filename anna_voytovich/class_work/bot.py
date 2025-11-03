6a3c6e30a441ec3761a41259c7ddb4e7a8eba772
Anna <avoytovich841@gmail.com>
2025-11-03 11:18:39 +0300

    update the text in commands "about" and "start"

--- anna_voytovich/class_work/bot.py
+++ anna_voytovich/class_work/bot.py
@@ -1,17 +1,16 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-API_TOKEN = '8484773386:AAGpNZ4tFuub5-xvCX28UKa-MLNbek7nwcE'
+API_TOKEN = '8353078171:AAEC9OJgRfq1gIG6n2Uhb8YbvJKaalaRcXM'
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Ола амиго! Я бот-помощник, чтобы узнать подробную инфу напиши /about')
+    await update.message.reply_text('Доброго времени суток! Я дипломный проект, созданный для запуска тестов, чтобы узнать подробную инфу напиши /about')
 
 
 async def about(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    about_text = 'Я @bto03942734_bot, который запускает тесты из заданных категорий.\nМой создатель - @anvoyt.\n Научилась создавать телеграм-бота и вот он есть я.\nПожелаем ей всего лучшего и ты можешь идти дальше кликать по кнопкам'
+    about_text = ('Я @dp2825_bot, дипломный проект моего создателя - @anvoyt.\nЯ запускаю тесты, которые ты можешь выбрать из заданных категорий и наблюдать за их выполнением.\nСписок команд можешь найти нажав кнопку "меню" в левом нижнем углу).\nЕще немного информации о создателе:\nЕе зовут Аня, учиться в IT Академии "ШАГ" на мануального и автоматизированного тестировщика.\nЕсли вдруг вам понадобиться тестировщик, можете с ней связаться:\n@anvoyt - telegram,\nhttps://www.linkedin.com/in/anna-voytovich-8543a322a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app - LinkedIn.')
     await update.message.reply_text(about_text)
-
 
 def main():
     app = Application.builder().token(API_TOKEN).build()
