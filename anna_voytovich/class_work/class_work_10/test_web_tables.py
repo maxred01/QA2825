fe023da4617b90c2b995eb255b2f9f99e78f0980
Anna <avoytovich841@gmail.com>
2025-11-18 18:06:52 +0300

    UI тесты для сайта demoqa webtables

--- anna_voytovich/class_work/class_work_10/test_web_tables.py
+++ anna_voytovich/class_work/class_work_10/test_web_tables.py
@@ -26,13 +26,49 @@ def test_selenium():
     department = driver.find_element(By.XPATH, '//input[@id="department"]')
     department.send_keys('QA')
     time.sleep(1)
-    driver.find_element(By.XPATH,'//button[@id="submit"]').click()
-
-
+    submit = driver.find_element(By.XPATH,'//button[@id="submit"]')
+    submit.click()
 
     time.sleep(2)
-    table_rows = driver.find_elements(By.XPATH, '//div[@class="rt-tr-group"]"]')
-    assert len(table_rows) == 3, f"Ожидалось 3 строки, но найдено {len(table_rows)}"
+
+    table_rows = driver.find_elements(By.XPATH, '//div[@role="rowgroup"]')
+
+    empty_rows = driver.find_elements(By.XPATH, '//div[@class="rt-tr -padRow -even"]') + driver.find_elements(By.XPATH, '//div[@class="rt-tr -padRow -odd"]')
+    filled_rows = len(table_rows) - len(empty_rows)
+
+    assert filled_rows == 4, f"Ожидалось 4 строки, но найдено {len(table_rows)}"
+
+    trash_1 = driver.find_element(By.XPATH, '//span[@id = "delete-record-1"]')
+    trash_1.click()
+
+    time.sleep(2)
 
 
+    name_cell = driver.find_element(By.XPATH, '(//div[@role="gridcell"])[15]')
+    assert name_cell.text == 'Anna', f'Неверный текст. Получено {name_cell}'
+    lastname_cell = driver.find_element(By.XPATH, '(//div[@role="gridcell"])[16]')
+    assert lastname_cell.text == 'Voyt', f'Неверный текст. Получен {lastname_cell}'
+    age_cell = driver.find_element(By.XPATH, '(//div[@role="gridcell"])[17]')
+    assert age_cell.text == '10', f'Неверное значение. Получено {age_cell}'
+    email_cell = driver.find_element(By.XPATH, '(//div[@role="gridcell"])[18]')
+    assert email_cell.text == 'anna@gmail.com', f'Неверный емаел. Получен {email_cell}'
+    salary_cell = driver.find_element(By.XPATH, '(//div[@role="gridcell"])[19]')
+    assert salary_cell.text == '200', f'Неверное значение. Получено {salary_cell}'
+    department_cell = driver.find_element(By.XPATH, '(//div[@role="gridcell"])[20]')
+    assert department_cell.text == 'QA', f'Неверный текст. Получено {department_cell}'
 
+    edit = driver.find_element(By.XPATH, '//span[@id = "edit-record-4"]')
+    edit.click()
+
+    time.sleep(2)
+
+    salary_edit = driver.find_element(By.XPATH,'//input[@id = "salary"]')
+    salary_edit.clear()
+    time.sleep(2)
+    salary_edit.send_keys('100000')
+    submit_edit = driver.find_element(By.XPATH, '//button[@id="submit"]')
+    submit_edit.click()
+    time.sleep(3)
+
+
+    assert salary_cell.text == '100000', f'Неверное значение. Получено {salary_cell}'
