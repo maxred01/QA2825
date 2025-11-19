47f3b529ac2b639062a93cba78132e7468755af6
ivanvikvik <ivanvikvik@gmail.com>
2025-11-19 21:47:06 +0300

    шалость

---
+++ max_tsybulka/class_work/class_work_12/locators/service_services/service_services.py
@@ -0,0 +1,15 @@
+import os
+
+from max_tsybulka.class_work.class_work_12.page.base_page import WebPage
+from max_tsybulka.class_work.class_work_12.page.elements import WebElement
+from max_tsybulka.class_work.class_work_12.page.elements import ManyWebElements
+
+
+class MainPage(WebPage):
+    def __init__(self, web_driver, url=''):
+        if not url:
+            url = os.getenv('MAIN_PAGE') or 'http://www.scania-minsk.by/'
+
+        super().__init__(web_driver, url)
+
+    btn_header_service_services = WebElement(xpath='(//a[@href="/service"])[1]')
