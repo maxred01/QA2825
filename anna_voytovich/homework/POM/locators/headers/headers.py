fa92e9c3830009a70c4af8a9fc2b321aee29814a
Anna <avoytovich841@gmail.com>
2025-11-20 18:56:02 +0300

    writing test for web site headers with POM

---
+++ anna_voytovich/homework/POM/locators/headers/headers.py
@@ -0,0 +1,18 @@
+import os
+from anna_voytovich.homework.POM.page.base_page import WebPage
+from anna_voytovich.homework.POM.page.elements import WebElement
+from anna_voytovich.homework.POM.page.elements import ManyWebElements
+
+
+class MainPage(WebPage):
+    def __init__(self, web_driver, url=''):
+        if not url:
+            url = os.getenv('MAIN_PAGE') or 'https://superjet.rostsayt.ru/#'
+
+        super().__init__(web_driver, url)
+
+    btn_header_advantages = WebElement(xpath='(//*[@aria-current="page" and @href="#advantagers"])[1]')
+    btn_header_effectivity = WebElement(xpath='(//header//li)[2]//*[@aria-current="page"]')
+    btn_header_complectation = WebElement(xpath='(//header//ul//*[@aria-current="page"])[3]')
+    btn_header_comfort = WebElement(xpath='(//nav//ul//*[@aria-current="page" and @href="#comforts"])[1]')
+    btn_header_buy = WebElement(xpath='(//header//ul//*[@aria-current="page" and @href="#feedbacks"])[1]')
